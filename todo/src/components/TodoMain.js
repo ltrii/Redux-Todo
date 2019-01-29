ba7054b63c55fb5e7c7e0519c4f3424c0cbeb07b
ltrii <lelandtr@gmail.com>
2019-01-29 08:21:37 -0800

    Store framework (redux). Actions defined. File structure set up.

---
+++ todo/src/components/TodoMain.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class TodoMain extends Component {
+    constructor() {
+        super();
+        this.state = {
+            body: ''
+        }
+    }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Todo</h1>
+      </div>
+    )
+  }
+}
+
+export default TodoMain;
